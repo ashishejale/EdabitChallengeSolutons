110d8e81162cf95a8eee3cbfdac16a8670bd11f6
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-17 14:44:33 +0530

    Update Return the Next Number from the Integer Passed.java

--- Java/Very Easy/Return the Next Number from the Integer Passed.java
+++ Java/Very Easy/Return the Next Number from the Integer Passed.java
@@ -1,5 +1,7 @@
 public class Challenge {
 	public static int addition(int num) {
-		return (num + 1); //for some reason the edabit tests do not pass ' return num++; '
+		return ++num; // means increment num and then return it
+		//or use 'return (num + 1);'  
+		//'return num++;' means return num then increment it
 	}
 }
